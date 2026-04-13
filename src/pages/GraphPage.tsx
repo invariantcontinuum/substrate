71e54fe26befd1210aa1342c86a5f6a1ff4c30d8
danycrafts <daniyal.ibrahim10@gmail.com>
2026-04-13 02:29:40 +0200

    fix(frontend): add padding around graph canvas

--- src/pages/GraphPage.tsx
+++ src/pages/GraphPage.tsx
@@ -4,7 +4,7 @@ import { NodeDetailPanel } from "@/components/graph/NodeDetailPanel";
 export function GraphPage() {
   return (
     <div className="h-full flex overflow-hidden">
-      <div className="flex-1 min-w-0 h-full">
+      <div className="flex-1 min-w-0 h-full p-2">
         <GraphCanvas />
       </div>
       <NodeDetailPanel />
